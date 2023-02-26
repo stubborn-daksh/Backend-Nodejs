aa6058d0d39aef3cbb71b5a3b5bf50e4d0757993
= <=>
2023-02-27 01:21:33 +0530

    Hello-World-program sun successfully on localhost/server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Backend-Nodejs
+
+#writefileSync() of fs module is used to create a one file with some written content which i want to write in that particular file, it contains two arguments -first => file name , -second=> text, which i want to write in this file
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// console.log("Hello World");
+const fs = require('fs');
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(3000);
+
+fs.writeFileSync('hello.txt', 'Hello  from Node.js');
