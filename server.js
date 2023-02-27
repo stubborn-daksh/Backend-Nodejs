48aa7645af5c4bfe14986b977b85f9c03ecdde32
= <=>
2023-02-27 23:47:17 +0530

    creating a server and run it on port 4000

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // console.log("Hello World");
-const fs = require('fs');
+// const fs = require('fs');
 var http = require('http');
-
+ 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(3000);
+  res.end('Daksh Kesarwani');
+}).listen(4000);
 
-fs.writeFileSync('hello.txt', 'Hello  from Node.js');
