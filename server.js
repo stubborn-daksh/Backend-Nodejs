b8657d7061e3b1335a53e0987cbc47799db0a8b4
= <=>
2023-03-01 23:30:39 +0530

    print a welcome message on the server

--- server.js
+++ server.js
@@ -2,8 +2,23 @@
 // const fs = require('fs');
 var http = require('http');
  
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Daksh Kesarwani');
+// http.createServer(function (req, res) {
+//     const url = req.url;
+//     if(url === '/home'){
+//       res.write('Welcome home');
+//     } else if(url === '/about'){
+//       res.write('Welcome to About us page');
+//     } else if(url === '/node'){
+//       res.write('Wrlcome to Node js page');
+//     }
+
+// }).listen(4000);
+
+http.createServer(function (req,res) {
+    res.setHeader('content-Type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
 }).listen(4000);
 
